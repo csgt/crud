832dae28c144009344f6b072219afc37655c3e3d
Jose Galindo (Desktop) <jgalindo@cs.com.gt>
2014-11-12 01:05:28 -0600

    Ocho colocho

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -36,7 +36,7 @@
 			    			$urlarr = explode('{id}', $url);
 			    			$parte1 = $urlarr[0];
 			    			$parte2 = (count($urlarr)==1?'':$urlarr[1]);
-			    			if (strpos($url, '?')===true) 
+			    			if (strpos($url, '?')!==false) 
 			    				$getVars = '?' . str_replace('?', '&', substr($getVars,1));
 			    		?>
 							html += '<a class="btn btn-xs btn-{{$botonExtra["class"]}}" title="{{$botonExtra["titulo"]}}" href="{{$parte1}}' + id + '{{$parte2 . $getVars}}"><span class="{{$botonExtra["icon"]}}"></span></a>';
