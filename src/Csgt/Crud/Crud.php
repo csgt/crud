665aa460f2653b97e61d9409b29046e38e11fe5e
Jose Galindo (Desktop) <jgalindo@cs.com.gt>
2015-01-15 18:36:53 -0600

    Explode estaba al reves los parametros

--- src/Csgt/Crud/Crud.php
+++ src/Csgt/Crud/Crud.php
@@ -362,12 +362,12 @@ class Crud {
 			else if ($campo['tipo']=='combobox')
 				$data[$campo['combokey']] = Input::get($campo['combokey']);
 			else if ($campo['tipo']=='date') {
-				$laFecha = explode(Input::get($campo['campoReal'],'/'));
+				$laFecha = explode('/',Input::get($campo['campoReal']));
 				$data[$campo['campoReal']] = $laFecha[2] . '-' . $laFecha[1] . '-' . $laFecha[0];
 			}
 			else if ($campo['tipo']=='datetime') {
-				$fechaHora = explode(Input::get($campo['campoReal'], ' '));
-				$laFecha = explode($fechaHora[0],'/');
+				$fechaHora = explode(' ', Input::get($campo['campoReal']));
+				$laFecha = explode('/',$fechaHora[0]);
 				$data[$campo['campoReal']] = $laFecha[2] . '-' . $laFecha[1] . '-' . $laFecha[0] . ' ' . $fechaHora[1];
 			}
 			else if ($campo['tipo']=='password') {
