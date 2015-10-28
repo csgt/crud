70fb26a313b528caaa2a6354d9263a1c6c812cf1
Erick Marroquin <emarroquin@cs.com.gt>
2015-10-27 18:49:12 -0600

    add feature de confirmar antes de procesar accion

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -33,18 +33,18 @@
 			    	var html = '';
 			    	@foreach ($botonesExtra as $botonExtra)
 			    		<?php 
-			    			$url = $botonExtra["url"];
-			    			$urlarr = explode('{id}', $url);
+			    			$url     = $botonExtra["url"];
+			    			$urlarr  = explode('{id}', $url);
 			    			$urlVars = '';
-			    			$parte1 = $urlarr[0];
-			    			$parte2 = (count($urlarr)==1?'':$urlarr[1]);
+			    			$parte1  = $urlarr[0];
+			    			$parte2  = (count($urlarr)==1?'':$urlarr[1]);
 			    			if ($nuevasVars!='') {
 			    				$urlVars = (strpos($url, '?')===false?'?':'&') . substr($nuevasVars,1);
 			    			}
 			    			$target = $botonExtra["target"];
 			    			if ($target<>'') $target='target="' . $target . '"';
 			    		?>
-							html += '<a class="btn btn-xs btn-{{$botonExtra["class"]}}" title="{{$botonExtra["titulo"]}}" href="{{$parte1}}' + id + '{{$parte2 . $urlVars}}" {{$target}}><span class="{{$botonExtra["icon"]}}"></span></a>';
+							html += '<a class="btn btn-xs btn-{{$botonExtra["class"]}}" title="{{$botonExtra["titulo"]}}" href="{{$parte1}}' + id + '{{$parte2 . $urlVars}}" {{$target}} {{ $botonExtra["confirm"] ? "onclick=\"return confirm(\'".$botonExtra["confirmmessage"]."\');\"" : "" }}><span class="{{$botonExtra["icon"]}}"></span></a>';
 						@endforeach
 			    	@if($permisos['edit'])   	
 							html += '<a class="btn btn-xs btn-primary" title="Editar" href="{{ URL::to(Request::url())}}/' + id + '/edit/{{$nuevasVars}}"><span class="glyphicon glyphicon-pencil"></span></a>';
